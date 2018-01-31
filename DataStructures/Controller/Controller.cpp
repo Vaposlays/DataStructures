ae80f8babaecd4a7e285dfb929cf6fe3e3052a2c
Mason Childers <31513862+Vaposlays@users.noreply.github.com>
2018-01-31 07:45:29 -0700

    created project and added all the files

---
+++ DataStructures/Controller/Controller.cpp
@@ -0,0 +1,14 @@
+//
+//  Controller.cpp
+//  DataStructures
+//
+//  Created by Childers, Mason on 1/31/18.
+//  Copyright © 2018 Childers, Mason. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+void Controller :: start()
+{
+    
+}
---
+++ DataStructures/Runner.cpp
@@ -0,0 +1,16 @@
+//
+//  Runner.cpp
+//  DataStructures
+//
+//  Created by Childers, Mason on 1/31/18.
+//  Copyright © 2018 Childers, Mason. All rights reserved.
+//
+
+#include "Controller/Controller.hpp"
+
+int main()
+{
+    Controller app;
+    app.start();
+    return 0;
+}
