fdde0c6917522c073a83a2d86478a44fbb7e4f33
Mason Childers <31513862+Vaposlays@users.noreply.github.com>
2018-02-06 09:03:45 -0700

    stuff

---
+++ DataStructures/Testers/ArrayTester.cpp
@@ -0,0 +1,39 @@
+//
+//  ArrayTester.cpp
+//  DataStructures
+//
+//  Created by Childers, Mason on 2/6/18.
+//  Copyright © 2018 Childers, Mason. All rights reserved.
+//
+
+#include "ArrayTester.hpp"
+
+void ArrayTester :: testArrayUse()
+{
+    Array<int> firstArray(10);
+    
+    for (int index = 0; index < 10; index++)
+    {
+        firstArray.setAtIndex(index, index);
+    }
+    
+    for (int index = 0; index < firstArray.getSize(); index++)
+    {
+        cout << firstArray[index] << endl;
+    }
+    
+    Array<int> secondArray(400);
+    
+    cout << secondArray.getSize() << endl;
+    cout << firstArray.getSize() << endl;
+    
+    Array<int> thirdArray(secondArray);
+    
+    cout << thirdArray.getSize() << endl;
+    firstArray = thirdArray;
+    cout << firstArray[4] << endl;
+    
+    thirdArray[5] = 123;
+    cout << thirdArray[5] << endl;
+    
+}
---
+++ DataStructures/Testers/ArrayTester.hpp
@@ -0,0 +1,24 @@
+//
+//  ArrayTester.hpp
+//  DataStructures
+//
+//  Created by Childers, Mason on 2/6/18.
+//  Copyright © 2018 Childers, Mason. All rights reserved.
+//
+
+#ifndef ArrayTester_hpp
+#define ArrayTester_hpp
+
+#include <iostream>
+#include "../Model/Structures/Array.hpp"
+#include "../Controller/FileController.hpp"
+#include "../Controller/Tools/Timer.hpp"
+
+#endif /* ArrayTester_hpp */
+
+class ArrayTester
+{
+public:
+    void testArrayUse();
+    void testAdvancedArray();
+}
