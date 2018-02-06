0080f4662bd92e9b22cc5059c3aad16f4a65a409
Mason Childers <31513862+Vaposlays@users.noreply.github.com>
2018-02-06 09:54:10 -0700

    added Linear node into node.hpp

--- DataStructures/Model/Nodes/Node.hpp
+++ DataStructures/Model/Nodes/Node.hpp
@@ -8,6 +8,7 @@
 
 #ifndef Node_hpp
 #define Node_hpp
+#include Node.hpp
 
 template <class type>
 {
@@ -39,4 +40,50 @@ Type Node<Type> :: getData()
     return data;
 }
 
+template <class Type>
+class LinearNode : public Node<Type>
+{
+    //This section is available to the subclasses of LinearNode
+protected:
+    LinearNode<Type * next;
+public:
+    //Constructors
+    LinearNode();
+    LinearNode(Type data);
+    LinearNode(Type data, LinearNode<Type> * next);
+    //Methods
+    void setNextNode(LinearNode<Type> * next);
+    LinearNode<Type> * getNextNode();
+};
+    
+template <class Type>
+LinearNode<Type> :: LinearNode() : Node<Type>()
+{
+    this->next = nullptr;
+}
+    
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data) : Node<Type>(data)
+{
+    this->next = nullptr;
+}
+    
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data, LinearNode<Type> * next) : Node<Type>(data)
+{
+    this->next = next;
+}
+
+template <class Type>
+void LinearNode<Type> :: setNextNode(LinearNode<Type> * nextNodePointer)
+{
+    this->next = nextNodePointer;
+}
+    
+template <class Type>
+LinearNode<Type> * LinearNode<Type> :: getNextNode()
+{
+    return next;
+}
+    
 #endif /* Node_hpp */
